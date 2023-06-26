be2208ebcfe66cb5b6fd9df6053ac2c469b6d2aa
Yuri <y.frontdev@gmail.com>
2023-06-26 10:46:30 +0300

    add some style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,13 @@
-import Link from "next/link"
+import Link from "next/link";
 
 const Header = () => {
-  return <header>
-    <Link href="/">Home</Link>
-    <Link href="/blog">Blog</Link>
-    <Link href="/about_us">About</Link>
-  </header>
-}
+  return (
+    <header className="header">
+      <Link href="/">Home</Link>
+      <Link href="/blog">Blog</Link>
+      <Link href="/about_us">About</Link>
+    </header>
+  );
+};
 
-export { Header }
+export { Header };
